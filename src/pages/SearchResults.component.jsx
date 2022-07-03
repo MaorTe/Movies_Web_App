ae5cc7a57eff6e572128566d9dbf2b746546d9a3
maorte <maor884@gmail.com>
2022-07-03 18:54:15 +0300

    testing observer

--- src/pages/SearchResults.component.jsx
+++ src/pages/SearchResults.component.jsx
@@ -12,30 +12,16 @@ const SearchResults = () => {
    const loader = useRef(null);
 
    const [results, setResults] = useState([]);
+   const [searchQuery, setSearchQuery] = useState('');
+
    const params = useParams();
-
-   // @ts-ignore
    const query = params.query;
-   // @ts-ignore
    const type = params.type;
-
-   // useEffect for IntersectionObserver
-   useEffect(() => {
-      var options = {
-         root: null,
-         rootMargin: '300px',
-         threshold: 1.0,
-      };
-      // initialize IntersectionObserver
-      // and attaching to Load More div
-      const observer = new IntersectionObserver(handleObserver, options);
-      if (loader.current) {
-         observer.observe(loader.current);
-      }
-   }, []);
 
    useEffect(() => {
       const search = async () => {
+         setSearchQuery(params.query);
+
          const dataMovies = await API.get(
             `3/search/${type}?api_key=b99ccc44cb21876b1925f3944e20854b&language=en-US&query=${query}&page=${page}&include_adult=false`,
          );
@@ -51,15 +37,34 @@ const SearchResults = () => {
          });
          setResults([...results, ...movies]);
       };
-      search();
-   }, [query, type, page]);
 
-   const handleObserver = async (entries) => {
-      const target = entries[0];
-      if (target.isIntersecting) {
-         setPage((page) => page + 1);
+      search();
+   }, [query, type, page, searchQuery]);
+
+   // useEffect for IntersectionObserver
+   useEffect(() => {
+      var options = {
+         root: null,
+         rootMargin: '200px',
+         threshold: 1.0,
+      };
+      // initialize IntersectionObserver
+      // and attaching to Load More div
+      const observer = new IntersectionObserver((entries) => {
+         const lastCard = entries[0];
+         if (lastCard.isIntersecting) {
+            setPage((page) => page + 1);
+         }
+      }, options);
+
+      if (query !== searchQuery) {
+         setResults([]);
+         console.log(page);
       }
-   };
+      if (loader.current) {
+         observer.observe(loader.current);
+      } else observer.unobserve(loader.current);
+   }, [query, searchQuery]);
 
    const onPosterClick = (movieID) => {
       console.log(movieID);
